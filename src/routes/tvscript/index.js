6b3b9d1a79e18ee25591fe61f2e0d0ca7a12c152
Jacob Polloreno <makaveliix16@gmail.com>
2017-12-01 12:36:50 -0800

    update tvscript page with URL

--- src/routes/tvscript/index.js
+++ src/routes/tvscript/index.js
@@ -4,18 +4,37 @@ import style from './style';
 
 
 export default class TVscript extends Component {
-	state = { primer: 'homer_simpson', text: '' };
+	state = { primer: 'homer_simpson:', text: '' };
 
-	setText = e => {
-		this.setState({ text: e.target.value });
-	};
+	// setText = e => {
+	// 	this.setState({ text: e.target.value });
+	// };
 
 	setSelect = e => {
 		this.setState({ primer: e.target.value });
 	};
 
 	handleSubmit = e => {
-		e.preventdefault();
+		let data = new FormData();
+		const URL = 'http://localhost:8000/predict_tvscript';
+
+		data.append('text', this.state.primer);
+
+		fetch(URL, {
+			method: 'POST',
+			body: data
+		}).then(
+			response => response.json()
+		).then(success => {
+			this.setState({
+				text: success[0].prediction_result
+			});
+
+			console.log(success);
+		}).catch(
+			error => console.log(error)
+		);
+
 	};
 
 	render({ }, { text, primer }) {
@@ -24,19 +43,13 @@ export default class TVscript extends Component {
 				<Grid.Cell class="mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone" />
 				<Grid.Cell class={'mdl-cell--8-col ' + style.demoContent} shadow="2">
 					<h3>Generate TV Script</h3>
-					<TextField
-						placeholder="Input some starter dialogue..."
-						value={text}
-						onInput={this.setText}
-						class={style.demoText}
-					/>
 					<select value={primer} onChange={this.setSelect}>
-						<option value="homer_simpson">Homer Simpson</option>
-						<option value="moe_szyslak">Moe Szyslak</option>
-						<option value="barney_gumble">Barney Gumble</option>
+						<option value="homer_simpson:">Homer Simpson</option>
+						<option value="moe_szyslak:">Moe Szyslak</option>
+						<option value="barney_gumble:">Barney Gumble</option>
 					</select>
-					<Button type="submit">Generate</Button>
-					<p>{ primer } : { text }</p>
+					<Button onClick={this.handleSubmit} type="submit">Generate</Button>
+					<p>{ text }</p>
 				</Grid.Cell>
 			</Grid>
 		);
