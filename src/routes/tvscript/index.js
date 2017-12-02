ec37a6ab649a53e72989f022fc66af20d09be303
Jacob Polloreno <makaveliix16@gmail.com>
2017-12-01 20:47:08 -0800

    fix tvscript gen with primer/text/speaker states

--- src/routes/tvscript/index.js
+++ src/routes/tvscript/index.js
@@ -4,21 +4,23 @@ import style from './style';
 
 
 export default class TVscript extends Component {
-	state = { primer: 'homer_simpson:', text: '' };
+	state = { speaker: 'homer_simpson:', primer: '', text: '' };
 
-	// setText = e => {
-	// 	this.setState({ text: e.target.value });
-	// };
+	setPrimer = e => {
+		this.setState({ primer: e.target.value,
+			text: '' });
+	};
 
 	setSelect = e => {
-		this.setState({ primer: e.target.value });
+		this.setState({ speaker: e.target.value });
 	};
 
 	handleSubmit = e => {
 		let data = new FormData();
 		const URL = 'http://localhost:8000/predict_tvscript';
 
-		data.append('text', this.state.primer);
+		let text = this.state.speaker + ' ' + this.state.primer;
+		data.append('text', text);
 
 		fetch(URL, {
 			method: 'POST',
@@ -27,29 +29,42 @@ export default class TVscript extends Component {
 			response => response.json()
 		).then(success => {
 			this.setState({
+				primer: '',
 				text: success[0].prediction_result
 			});
-
-			console.log(success);
 		}).catch(
 			error => console.log(error)
 		);
 
 	};
 
-	render({ }, { text, primer }) {
+	render({ }, { speaker, text, primer }) {
 		return (
 			<Grid class={style.demoContainer}>
 				<Grid.Cell class="mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone" />
 				<Grid.Cell class={'mdl-cell--8-col ' + style.demoContent} shadow="2">
-					<h3>Generate TV Script</h3>
-					<select value={primer} onChange={this.setSelect}>
+					<h3>Generate Short TV Script</h3>
+					<TextField
+						placeholder="Input some starter dialogue..."
+						value={primer}
+						onInput={this.setPrimer}
+						class={style.demoText}
+						maxlength={20}
+					/>
+					<select value={speaker} onChange={this.setSelect}>
 						<option value="homer_simpson:">Homer Simpson</option>
 						<option value="moe_szyslak:">Moe Szyslak</option>
 						<option value="barney_gumble:">Barney Gumble</option>
 					</select>
 					<Button onClick={this.handleSubmit} type="submit">Generate</Button>
-					<p>{ text }</p>
+					<div>
+						{ primer ? speaker : null} { primer }
+					</div>
+					<div>
+						{ text.split('\n').map(line => (
+							<p>{ line }</p>
+						))}
+					</div>
 				</Grid.Cell>
 			</Grid>
 		);
